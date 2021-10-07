5dcdc5cd28974dec8b921e56d4b2e934eb871e5d
Vinayak <kumarvinayak490@gmail.com>
2021-10-07 14:55:13 +0530

    initial commit

---
+++ loop.js
@@ -0,0 +1,21 @@
+/*let i=0;
+
+do{
+    console.log("The number is "+i);
+    i++;
+
+}
+while(i>5);*/
+
+let a=['Joey','Chandeler','Ross','Rachel','Monica','Phoebe'];
+
+for (let i=0;i<a.length;i++){
+
+console.log(a[i]);
+
+}
+
+
+
+
+
---
+++ switch.js
@@ -0,0 +1,19 @@
+let marks=10;
+
+switch(marks){
+
+    case 50:
+        console.log("You failed");
+        break;
+    case 30:
+        console.log("you failed miserably");
+        break;
+    case 90:
+        console.log("Wah! mere heere");
+        break;
+
+    default:
+        console.log("Keep it up");
+        break;
+
+}
